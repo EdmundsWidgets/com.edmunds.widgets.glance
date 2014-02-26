f490198f4d58fc3ba5ad078b404cbf86e7d5cbda
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2014-02-26 18:52:29 +0300

    added unit test example

---
+++ test/unit/example.spec.js
@@ -0,0 +1,17 @@
+define([
+    'utils'
+], function(utils) {
+
+    describe('utils', function() {
+
+        it('returnA', function() {
+            expect(utils.returnA()).toBe('A');
+        });
+
+        it('sum', function() {
+            expect(utils.sum(1, 2)).toBe(3);
+        });
+
+    });
+
+});
