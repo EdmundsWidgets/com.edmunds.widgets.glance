ad49b83255679535a3e0adbfade5f40f95cc091d
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-02 17:10:58 +0300

    Reformated template.

--- app/js/template/vehicle/rating-details.js
+++ app/js/template/vehicle/rating-details.js
@@ -1,3 +1,38 @@
 define(function() {
-    return _.template('<div class="row"> <div class="rating-details"> <div class="rating-title col-xs-12"> <div class="letter-grade small excellent">a</div> <div class="category">Comfort</div> <div class="text-grade">Excellent</div> <div class="close-button"></div> </div> <hr class="col-sm-12"/> <!--note: Delete this porn later--> <p class="description">It\'s when you begin to look at what else is available for around 026K that the ILX really shines. Its generous standard equipment and competitive fuel economy (not to mention good looks) should give it some traction in the market.</p> <div class="edmunds-rating-strips"> <div class="col-xs-12 col-sm-6 col-md-4"> <p>A<span class="strip"></span>Build Quality (vs. $)</p> </div> <div class="col-xs-12 col-sm-6 col-md-4"> <p>A<span class="strip"></span>Features (vs. $)</p> </div> <div class="col-xs-12 col-sm-6 col-md-4"> <p>A<span class="strip"></span>Cost</p> </div> <div class="col-xs-12 col-sm-6 col-md-4"> <p>A<span class="strip"></span>MPG</p> </div> <div class="col-xs-12 col-sm-6 col-md-4"> <p>B<span class="strip good"></span>Warranty</p> </div> <div class="col-xs-12 col-sm-6 col-md-4"> <p>B<span class="strip good"></span>Ownership</p> </div> </div> <div class="col-xs-12"> <a href="#" class="btn btn-primary btn-primary-dark">Read full review <span class="hidden-xs">on Edmunds.com</span></a> </div> </div> </div>')
+    return _.template('' +
+        '<div class="rating-details">' +
+        '<div class="rating-title col-xs-12">' +
+        '<div class="letter-grade small excellent">a</div>' +
+        '<div class="category">Comfort</div>' +
+        '<div class="text-grade">Excellent</div>' +
+        '<div class="close-button"></div>' +
+        '</div>' +
+        '<hr class="col-sm-12"/>' +
+        '<!--note: Delete this porn later-->' +
+        '<p class="description">It\'s when you begin to look at what else is available for around 026K that the ILX really shines. Its generous standard equipment and competitive fuel economy (not to mention good looks) should give it some traction in the market.</p>' +
+        '<div class="edmunds-rating-strips">' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>A<span class="strip"></span>Build Quality (vs. $)</p>' +
+        '</div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>A<span class="strip"></span>Features (vs. $)</p>' +
+        '</div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>A<span class="strip"></span>Cost</p>' +
+        '</div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>A<span class="strip"></span>MPG</p>' +
+        '</div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>B<span class="strip good"></span>Warranty</p>' +
+        '</div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>B<span class="strip good"></span>Ownership</p>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-xs-12">' +
+        '<a href="#" class="btn btn-primary btn-primary-dark">Read full review <span class="hidden-xs">on Edmunds.com</span></a>' +
+        '</div>' +
+        '</div>' +
+    '')
 });
