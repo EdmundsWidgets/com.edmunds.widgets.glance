a676895550071b39abc844fc70fbaf8b56861c76
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 17:01:46 +0300

    Added values from the model to the template.

--- app/js/template/vehicle/rating-selector.js
+++ app/js/template/vehicle/rating-selector.js
@@ -2,10 +2,10 @@ define(function() {
     return _.template('' +
         '<div class="col-xs-12 col-sm-6 col-md-4">' +
         '<div data-id="<%= id %>" class="rating-selector">' +
-        '<div class="letter-grade small excellent"><%= grade %></div>' +
+        '<div class="letter-grade small <%= grade.gradeClass %>"><%= grade.grade %></div>' +
         '<div class="category"><%= title %></div>' +
         '<div class="arrow-right"></div>' +
-        '<div class="text-grade">Excellent</div>' +
+        '<div class="text-grade"><%= grade.textGrade %></div>' +
         '</div>' +
         '</div>' +
     '');
