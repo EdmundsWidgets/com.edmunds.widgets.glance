bc6d122041ffda195ef2998f35b79dfd134da9b6
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 12:26:10 +0300

    Refactor

--- app/js/collection/vehicle/rating.js
+++ app/js/collection/vehicle/rating.js
@@ -1,14 +1,10 @@
 define([
     'model/vehicle/rating'
 ], function(RatingModel) {
-
     return Backbone.Collection.extend({
-
         model: RatingModel,
-
         comparator: function(model) {
             return model.get('title');
         }
-
     });
 });
