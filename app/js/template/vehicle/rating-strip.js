a71a7f51acbff09cf94e6b3f67b2d14967c16b25
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 18:17:06 +0300

    Created rating-strip teamplate

---
+++ app/js/template/vehicle/rating-strip.js
@@ -0,0 +1,9 @@
+define(function() {
+    return _.template('' +
+        '<div class="edmunds-rating-strips">' +
+        '<div class="col-xs-12 col-sm-6 col-md-4">' +
+        '<p>A<span class="strip"></span>Build Quality (vs. $)</p>' +
+        '</div>' +
+        '</div>' +
+    '');
+});
