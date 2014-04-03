9481e57fdf619dc4ce6e95f8477fdc581a2ca79a
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 16:59:59 +0300

    Added convertGrade method. It returns object with needed classes and values for the RatingSelector view

--- app/js/model/vehicle/rating.js
+++ app/js/model/vehicle/rating.js
@@ -9,7 +9,45 @@ define([
                 parse: true
             });
 
+            response.grade = this.convertGrade(response.grade);
+
             return response;
+        },
+        convertGrade: function(grade) {
+            switch (grade.toLowerCase()) {
+                case 'a':
+                    return {
+                        grade: 'a',
+                        textGrade: 'Excellent!',
+                        gradeClass: 'excellent'
+                    };
+                case 'b':
+                    return {
+                        grade: 'b',
+                        textGrade: 'Good',
+                        gradeClass: 'good'
+                    };
+                case 'c':
+                    return {
+                        grade: 'c',
+                        textGrade: 'Fair',
+                        gradeClass: 'fair'
+                    };
+                case 'd':
+                    return {
+                        grade: 'd',
+                        textGrade: 'Poor',
+                        gradeClass: 'poor'
+                    };
+                case 'e':
+                    return {
+                        grade: 'e',
+                        textGrade: 'Bad',
+                        gradeClass: 'bad'
+                    };
+                default:
+                    return 'N/A';
+            }
         }
     });
 });
