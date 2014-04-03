3f784b530ca9b56cd592f5104dbbedb8a6c415ab
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 19:14:19 +0300

    Added sub-rating model.

--- app/js/collection/vehicle/sub-rating.js
+++ app/js/collection/vehicle/sub-rating.js
@@ -1,5 +1,8 @@
-define(function() {
+define([
+    'model/vehicle/sub-rating'
+], function(SubRatingModel) {
     return Backbone.Collection.extend({
+        model: SubRatingModel,
         comparator: function(model) {
             return model.get('title');
         }
--- app/js/template/vehicle/rating-strip.js
+++ app/js/template/vehicle/rating-strip.js
@@ -1,9 +1,11 @@
 define(function() {
     return _.template('' +
-        '<div class="edmunds-rating-strips">' +
         '<div class="col-xs-12 col-sm-6 col-md-4">' +
-        '<p>A<span class="strip"></span>Build Quality (vs. $)</p>' +
+        '<div class="grade"><%= grade.grade %></div>' +
+        '<div class="strip-container">' +
+        '<div class="strip <%= grade.gradeClass %>" style="width: ' + '<%= score * 10 %>' + '%"></div>' +
         '</div>' +
+        '<div class="title"><%= title %></div>' +
         '</div>' +
     '');
 });
