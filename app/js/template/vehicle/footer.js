7d25bd3782633219e80117bd9cd27ce046fd258b
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 19:23:46 +0300

    Added footer template

---
+++ app/js/template/vehicle/footer.js
@@ -0,0 +1,9 @@
+define(function() {
+    return _.template('<footer class="container-fluid">' +
+        '<div class="row">' +
+        '<a class="legacy col-xs-5" href="#">Legal Notice</a>' +
+        '<div class="logo col-xs-7"><a href="http://www.edmunds.com" class="pull-right"></a>Build by</div>' +
+        '</div>' +
+        '</footer>' +
+    '');
+});
