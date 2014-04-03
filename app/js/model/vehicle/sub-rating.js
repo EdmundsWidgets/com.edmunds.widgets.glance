7d73f019bbb69650321e9b9f2d7d88349ce9da07
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 18:25:21 +0300

    Removed unnecessary SubRatingModel

--- app/js/model/vehicle/sub-rating.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-    return Backbone.Model.extend({
-        parse: function(response) {
-            response.id = response.title.toLowerCase().replace(/\s+/g, '-');
-
-            return response;
-        }
-    });
-});
