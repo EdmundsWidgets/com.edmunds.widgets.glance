b10105b8443a3ea55b9bf2b55dad25f2a3dd9486
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 13:18:39 +0300

    Refactor main parse method for the grade model

--- app/js/model/vehicle/grade.js
+++ app/js/model/vehicle/grade.js
@@ -1,64 +1,16 @@
 define([
-    'collections/vehicle/rating'
+    'collection/vehicle/rating'
 ], function(RatingCollection) {
     return Backbone.Model.extend({
         url: function(styleId) {
             return 'https://api.edmunds.com/api/vehicle/v2/grade/' + styleId;
         },
         parse: function(response) {
-            this.ratings = new RatingCollection(response.ratings, {
+            response.ratings = new RatingCollection(response.ratings, {
                 parse: true
             });
+
+            return response;
         }
     });
 });
-
-
-/*define(['collection/vehicle/ratings'], function(RatingsCollection) {
-    return Backbone.Model.extend({
-        url: function(styleId) {
-            return 'https://api.edmunds.com/api/vehicle/v2/grade/' + styleId;
-        },
-        parse: function(response) {
-            response.ratings = new RatingsCollection(response.ratings);
-            response.grade = this.convertGrade(response.grade);
-            return response;
-        },
-        convertGrade: function(grade) {
-            switch (grade.toLowerCase()) {
-                case 'a':
-                    return {
-                        grade: 'a',
-                        textGrade: 'Excellent!',
-                        gradeClass: 'excellent'
-                    };
-                case 'b':
-                    return {
-                        grade: 'b',
-                        textGrade: 'Good',
-                        gradeClass: 'good'
-                    };
-                case 'c':
-                    return {
-                        grade: 'c',
-                        textGrade: 'Fair',
-                        gradeClass: 'fair'
-                    };
-                case 'd':
-                    return {
-                        grade: 'd',
-                        textGrade: 'Poor',
-                        gradeClass: 'poor'
-                    };
-                case 'e':
-                    return {
-                        grade: 'e',
-                        textGrade: 'Bad',
-                        gradeClass: 'bad'
-                    }
-                default:
-                    return 'N/A';
-            }
-        }
-    });
-});*/
