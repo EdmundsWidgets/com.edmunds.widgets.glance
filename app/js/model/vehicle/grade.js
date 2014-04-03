2c489095d1faad6a293a2eb4e4079227aa921a19
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-03 15:37:16 +0300

    Added convertGrade method. It parses grade value and return object that contains needed values of classes and text grade.

--- app/js/model/vehicle/grade.js
+++ app/js/model/vehicle/grade.js
@@ -10,7 +10,50 @@ define([
                 parse: true
             });
 
+            response.grade = this.convertGrade(response.grade);
+
             return response;
+        },
+        convertGrade: function(grade) {
+            switch (grade.toLowerCase()) {
+                case 'a':
+                    return {
+                        grade: 'a',
+                        textGrade: 'Excellent!',
+                        gradeClass: 'excellent',
+                        activeA: 'active'
+                    };
+                case 'b':
+                    return {
+                        grade: 'b',
+                        textGrade: 'Good',
+                        gradeClass: 'good',
+                        activeB: 'active'
+                    };
+                case 'c':
+                    return {
+                        grade: 'c',
+                        textGrade: 'Fair',
+                        gradeClass: 'fair',
+                        activeC: 'active'
+                    };
+                case 'd':
+                    return {
+                        grade: 'd',
+                        textGrade: 'Poor',
+                        gradeClass: 'poor',
+                        activeD: 'active'
+                    };
+                case 'e':
+                    return {
+                        grade: 'e',
+                        textGrade: 'Bad',
+                        gradeClass: 'bad',
+                        activeE: 'active'
+                    };
+                default:
+                    return 'N/A';
+            }
         }
     });
 });
