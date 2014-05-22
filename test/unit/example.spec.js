b6e84c9a48ed912dba6d3299c0dc0a286fedcb92
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-05-22 15:15:32 +0300

    Created bower.json config and added main dependencies.

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -1,17 +1,17 @@
-define([
-    'example' // src/example.js
-], function(utils) {
-
-    describe('utils', function() {
-
-        it('returnA', function() {
-            expect(utils.returnA()).toBe('A');
-        });
-
-        it('sum', function() {
-            expect(utils.sum(1, 2)).toBe(3);
-        });
-
-    });
-
-});
+//define([
+//    'example' // src/example.js
+//], function(utils) {
+//
+//    describe('utils', function() {
+//
+//        it('returnA', function() {
+//            expect(utils.returnA()).toBe('A');
+//        });
+//
+//        it('sum', function() {
+//            expect(utils.sum(1, 2)).toBe(3);
+//        });
+//
+//    });
+//
+//});
