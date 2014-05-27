30925de6d5628a87f725533b54f82502cc389fa1
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-05-27 15:05:23 +0300

    Fixed reviews list overlaps footer

--- src/js/template/consumer-reviews-tab/reviews-list.js
+++ src/js/template/consumer-reviews-tab/reviews-list.js
@@ -14,7 +14,7 @@ define(function() {
         '<span class="hidden-xs">Based on</span><span>12</span> reviews</div>' +
         '</div>' +
         '</div>' +
-        '<section class="content ">' +
+        '<section class="content col-xs-12">' +
         '<% _.each(collection, function(review) { %><div class="consumer-review" data-id="<%= review.id %>"><div class="container-fluid"><div class="row"><div class="col-xs-12 col-sm-10"><div class="row"><div class="col-xs-12"><div class="rating-stars"><div class="star filled"></div><div class="star filled"></div><div class="star filled"></div><div class="star filled"></div><div class="star"></div></div><div class="date"><%= review.created %></div> </div> <div class="col-xs-12"><h4><%= review.title %></h4> <p class="vehicle">Vehicle: 2014 Mazda MAZDA3 i Touring 4dr Hatchback (2.0L 4cyl 6A)</p></div> </div> </div> <div class="hidden-xs col-sm-2"> <div class="pros"><%= review.thumbsUpDownCounter.thumbsUp %></div> <div class="clearmine"></div> <div class="cons"><%= review.thumbsUpDownCounter.thumbsDown %></div> </div> </div> </div> </div> <% }) %>' +
         '</section>' +
     '');
