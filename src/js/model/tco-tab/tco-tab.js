f8264f3a822ca2c44587a40eae114eb99b08841e
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-06-03 18:37:51 +0300

    Added parse method to the TcoModel.

--- src/js/model/tco-tab/tco-tab.js
+++ src/js/model/tco-tab/tco-tab.js
@@ -1,5 +1,26 @@
 define(function() {
     return Backbone.Model.extend({
-        url: 'https://api.edmunds.com/api/tco/v1/details/allnewtcobystyleidzipandstate/200434856/90404/ca?fmt=json&api_key=axr2rtmnj63qsth3ume3tv5f'
+        url: 'https://api.edmunds.com/api/tco/v1/details/allnewtcobystyleidzipandstate/200434856/90404/ca?fmt=json&api_key=axr2rtmnj63qsth3ume3tv5f',
+        parse: function(response) {
+            for (var key in response) {
+                if (response.hasOwnProperty(key) && response[key].hasOwnProperty('values')) {
+                    response[key].values = this.currencyFormatting(response[key].values);
+                }
+            }
+            response.taxcredit = '$' + response.taxcredit;
+            return response;
+        },
+        currencyFormatting: function(listOfValues) {
+            for (var i = 0; i < listOfValues.length; i++) {
+                listOfValues[i] += '';
+                if (listOfValues[i].length > 3) {
+                    var a = listOfValues[i].substring(listOfValues[i].length - 3);
+                    listOfValues[i] = '$' + listOfValues[i].replace(a, ',' + a);
+                } else {
+                    listOfValues[i] = '$' + listOfValues[i];
+                }
+            }
+            return listOfValues;
+        }
     });
 });
