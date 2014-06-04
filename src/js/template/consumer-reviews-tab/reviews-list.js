b568cf4b0fbdc207ff2395d43916786082ecc5ba
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-06-04 13:16:09 +0300

    Big rating stars is filled according average consumer rating.

--- src/js/template/consumer-reviews-tab/reviews-list.js
+++ src/js/template/consumer-reviews-tab/reviews-list.js
@@ -3,11 +3,14 @@ define(function() {
         '<div class="rating-bar container-fluid">' +
         '<div class="row">' +
         '<div class="rating-stars col-xs-7 col-sm-8">' +
-        '<div class="star filled"></div>' +
-        '<div class="star filled"></div>' +
-        '<div class="star filled"></div>' +
-        '<div class="star filled"></div>' +
+        '<div class="big-stars">' +
         '<div class="star"></div>' +
+        '<div class="star"></div>' +
+        '<div class="star"></div>' +
+        '<div class="star"></div>' +
+        '<div class="star"></div>' +
+        '<div class="rating-strip" style="width: <%= averageRating * 20 %>%"></div>' +
+        '</div>' +
         '<div class="text-rating-star hidden-xs"><span><%= averageRating %></span> out of 5.0</div>' +
         '</div>' +
         '<div class="reviews-count col-xs-5 col-sm-4">' +
