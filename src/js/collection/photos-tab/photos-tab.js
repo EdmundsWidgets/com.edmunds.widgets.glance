5fb8571efc23701c894060985864d80ddaae67a6
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-06-05 16:20:59 +0300

    Added photos tab Collection

---
+++ src/js/collection/photos-tab/photos-tab.js
@@ -0,0 +1,5 @@
+define(function() {
+    return Backbone.Collection.extend({
+        url: 'http://www.edmunds.com/mobilerest/mobile-rest-media/2.0/media/honda/accord/2013/sedan?version=2'
+    });
+});
--- src/js/view/photos-tab/photos-tab.js
+++ src/js/view/photos-tab/photos-tab.js
@@ -1,11 +1,14 @@
 define([
-    'template/photos-tab/photos-tab'
-], function(photosTabTemplate) {
+    'template/photos-tab/photos-tab',
+    'collection/photos-tab/photos-tab'
+], function(photosTabTemplate, PhotosTabCollection) {
     return Backbone.View.extend({
         template: photosTabTemplate,
+        collection: new PhotosTabCollection(),
         initialize: function() {},
         render: function() {
             this.$el.html(this.template);
+            return this;
         }
     });
 });
